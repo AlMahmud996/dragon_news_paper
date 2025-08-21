6c21e1fba5a03928289c407ef1748e53b257643b
AlMahmud996 <siftiam@gmail.com>
2025-08-21 13:29:19 +0600

    Firebase Project Build and input users

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAb6V6msqq3S9u3NjGN6IoBn2okyATa9RI",
+  authDomain: "dragon-news-breaking-8b5aa.firebaseapp.com",
+  projectId: "dragon-news-breaking-8b5aa",
+  storageBucket: "dragon-news-breaking-8b5aa.firebasestorage.app",
+  messagingSenderId: "837520705458",
+  appId: "1:837520705458:web:6afe13537600cd393a86ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
